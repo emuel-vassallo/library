aec85a4ad4f06ef8939c647689c8ebfd3fc0db4e
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-14 13:00:08 +0200

    change card hover effect to border

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const getRandomColor = () =>
   'hsl(' +
   360 * Math.random() +
   ',' +
-  (7 + 10 * Math.random()) +
+  (4 + 9 * Math.random()) +
   '%,' +
   (76 + 10 * Math.random()) +
   '%)';
